ae941a8bf32ec359fa985324fc0307814669988d
hector <hector@localhost.localdomain>
2013-12-02 15:01:47 -0400

    Control de ejecutar query

--- controler/Connection.php
+++ controler/Connection.php
@@ -9,27 +9,28 @@ class Connection {
     private $user;
     private $password;
     private $library;
+    private $dbh;
 
     private function __construct() {
 
-       // $this->server = 'mysql:host=localhost;dbname=oxiris_biblioteca';
-       // $this->user = 'oxiris_bibli';
-      //  $this->password = 'biblioteca';
+// $this->server = 'mysql:host=localhost;dbname=oxiris_biblioteca';
+// $this->user = 'oxiris_bibli';
+//  $this->password = 'biblioteca';
         $this->server = 'mysql:host=localhost;dbname=biblioteca';
         $this->user = 'root';
-        $this->password = 'higher';
+        $this->password = 'quisquella';
 
         $structure = new NotORM_Structure_Convention(
-                $primary = "id_%s", // id_$tableitebc
+                $primary = "id_%s", // id_$table
                 $foreign = "id_%s", // id_$table
-                $table = "%ss", // {$table}se
+                $table = "%ss", // {$table}
                 $prefix = "" // wp_$table
         );
 
-        $dbh = new PDO($this->server, $this->user, $this->password, array(PDO::ATTR_PERSISTENT => true));
-        $this->library = new NotORM($dbh, $structure);
+        $this->dbh = new PDO($this->server, $this->user, $this->password, array(PDO::ATTR_PERSISTENT => true));
+        $this->library = new NotORM($this->dbh, $structure);
 //        $this->library->debug = false;
-      //  $this->library->debug=true;
+//        $this->library->debug=true;
     }
 
     public static function getInstance() {
@@ -41,6 +42,15 @@ class Connection {
 
     public function getLibrary() {
         return $this->library;
+    }
+
+    function query($query) {
+        try {
+            $stmt = $this->dbh->query($query);
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } catch (PDOException $ex) {
+        }
+        return null;
     }
 
 }
